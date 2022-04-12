1f5e16566c32fab0af536bac0eabed761d7f0e7f
Leonardo Coronel <leomcoronel99@gmail.com>
2022-04-12 17:52:55 -0300

    feature carousel de proyectos

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -2,30 +2,23 @@ import React, { useContext, useState } from 'react';
 import Section from '../Styled/Section';
 
 import { LanguageContext } from '../contexts/LanguageContext';
-import Project from '../components/Projects/Project';
-import { projectEs, projectEn } from '../components/Projects/projectData';
+import Carousel from '../components/Carousel/Carousel';
 
 const Skills = () => {
-
-    let projectData = projectEs;
     let title = "Proyectos";
 
     const { data } = useContext(LanguageContext);
 
     if(data.locale === 'es') {
-        projectData = projectEs;
         title = "Proyectos";
     } else if(data.locale === 'en') {
-        projectData = projectEn;
         title = "Projects"
     }
 
     return (
         <Section id="portfolio">
             <h1>{title}</h1>
-            {projectData.map((project, index) => {
-                return <Project key={index} title={project.title} desc={project.desc} techs={project.techs} img={project.img} links={project.links} />
-            })}
+            <Carousel />
         </Section>
         )
 }
